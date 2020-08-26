0dc3b1741c7ee5e4d2d0d6beea82a8b789710dcf
Isaac Gorman <emailme@isaacg.net>
2020-08-26 17:04:36 -0500

    initial commit, created react app, added prettier config. Ready for note taking

---
+++ prj/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>React Testing</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;

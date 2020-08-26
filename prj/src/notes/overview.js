35c3a6057549d4bd917675c9361b17c89a0517d9
Isaac Gorman <emailme@isaacg.net>
2020-08-26 17:16:46 -0500

    overview annotation complete

---
+++ prj/src/notes/overview.js
@@ -0,0 +1,7 @@
+// Module Overview
+// - In this module I will be introduced to some common testing techniques used for React components
+
+// Learning Objectives
+// - I should be able to test props as they change from React components
+// - I will be able to use "mocks" in web application test
+// - I will be capable of testing asyn API calls that are made from within components
